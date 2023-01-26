28061b687a8131e016ca64175bcacca442c1a013
Luke Wong <lukewong93@gmail.com>
2023-01-26 21:22:17 +0800

    refactor: improve code organisation

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import { Invoice } from "./classes/Invoice.js";
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#toform") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
